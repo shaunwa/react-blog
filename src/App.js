73b97c662bc98f26e3ff4e9cff77123835dd8559
Shaun Wassell <shaun@trove.ai>
2020-02-01 18:27:53 -0500

    01.05 - Added NavBar and content styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import NavBar from './NavBar';
 import AboutPage from './pages/AboutPage';
 import ArticlePage from './pages/ArticlePage';
 import ArticlesListPage from './pages/ArticlesListPage';
@@ -10,10 +11,13 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Route path="/" exact component={HomePage} />
-        <Route path="/about" component={AboutPage} />
-        <Route path="/articles" exact component={ArticlesListPage} />
-        <Route path="/articles/:id" component={ArticlePage} />
+        <NavBar />
+        <div id="page-body">
+          <Route path="/" exact component={HomePage} />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/articles" exact component={ArticlesListPage} />
+          <Route path="/articles/:id" component={ArticlePage} />
+        </div>
       </div>
     </Router>
   );
---
+++ src/NavBar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NavBar = () => (
+    <nav>
+        <ul>
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            <li>
+                <Link to="/about">About</Link>
+            </li>
+            <li>
+                <Link to="/articles">Articles</Link>
+            </li>
+        </ul>
+    </nav>
+);
+
+export default NavBar;
