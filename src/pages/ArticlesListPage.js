f4ac99854a17d502f56921a2494922a3d1e2f7e9
Shaun Wassell <shaun@trove.ai>
2020-02-02 08:36:06 -0500

    01.07 - Implemented ArticleListPage

--- src/pages/ArticlesListPage.js
+++ src/pages/ArticlesListPage.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import articles from './article-content';
 
 const ArticlesListPage = () => (
-    <h1>Hi, I'm the articles list page!</h1>
+    <>
+    <h1>Articles</h1>
+    {articles.map((article, key) => (
+        <Link className="article-list-item" to={`/articles/${article.name}`} key={key}>
+            <h3>{article.title}</h3>
+            <p>{article.content[0].substring(0, 150)}...</p>
+        </Link>
+    ))}
+    </>
 );
 
 export default ArticlesListPage;
