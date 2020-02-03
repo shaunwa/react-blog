ff9b8a947a7b5f91cb6ed18fcfac9021c6e5cc18
Shaun Wassell <shaun@trove.ai>
2020-02-02 23:57:32 -0500

    04.02 - Added useState and useEffect

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import articles from './article-content';
 
 const ArticlePage = ({ match }) => {
     const { name } = match.params;
+
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+    useEffect(() => {
+        setArticleInfo({ upvotes: 3 });
+    });
+
     const matchingArticle = articles.find(article => article.name === name);
     return matchingArticle ? 
     (
         <>
         <h1>{matchingArticle.title}</h1>
+        <p>This article has {articleInfo.upvotes} upvotes</p>
         {matchingArticle.content.map((paragraph, key) => <p key={key}>{paragraph}</p>)}
         </>
     ) : (
